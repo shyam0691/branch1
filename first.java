ed422fbc34f35f66aa6226fcc417210c2b8e4d9d
shyam0691 <97492782+shyam0691@users.noreply.github.com>
2022-01-17 07:57:23 +0530

    Update first.java

--- first.java
+++ first.java
@@ -2,7 +2,7 @@ public class first {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("First git code");
+		System.out.println("First git code by shyam");
 	}
 
 }
